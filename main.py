23e27176812f38c385d4e94f5454acbfbfa5c27b
Kaydence Lin <kaydencelin@yahoo.com>
2025-04-01 12:52:52 -0400

    add flask

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 # Replace with your bot token
-SLACK_BOT_TOKEN = "xoxb-8401469809890-8424426773904-wsq9PmLqkHTO1UuSz44ZIQVK"
+#SLACK_BOT_TOKEN = "xoxb-8401469809890-8424426773904-wsq9PmLqkHTO1UuSz44ZIQVK"
 
 client = WebClient(token=SLACK_BOT_TOKEN)
 
