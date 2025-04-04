e9c5eb204ab0aab211aa6b198ace220eb53c11ab
matthewliii <matthewli4859@gmail.com>
2025-04-03 20:42:35 -0400

    for demos

--- qamodel.py
+++ qamodel.py
@@ -76,4 +76,4 @@ def test():
     return jsonify({"out": "Working"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6000, debug=True)  # Runs on port 5000
+    app.run(host="0.0.0.0", port=6000, debug=True)  # Runs on port 6000
